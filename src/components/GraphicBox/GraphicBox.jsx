c7c32a5f50e5f70188b352b874adac1a4186af0c
dxxglas <mcavalcante.douglas@gmail.com>
2020-07-08 13:45:37 -0300

    feature-grafico: mudança de cor e centralização do gráfico

--- src/components/GraphicBox/GraphicBox.jsx
+++ src/components/GraphicBox/GraphicBox.jsx
@@ -10,16 +10,19 @@ class GraphicBox extends Component {
       options: {
         chart: {
           type: "bar",
-          height: 240,
+          height: 300,
         },
+        colors: ["#755fe2"],
         plotOptions: {
           bar: {
-            horizontal: true,
+            horizontal: false,
             endingShape: "rounded",
+            barHeight: "100%",
           },
         },
         dataLabels: {
           enabled: false,
+          position: "bottom",
         },
         stroke: {
           show: true,
@@ -28,6 +31,9 @@ class GraphicBox extends Component {
         },
         xaxis: {
           categories: this.props.data.map((element) => element.genre),
+        },
+        fill: {
+          colors: ["#755fe2"],
         },
       },
       series: [
@@ -44,10 +50,11 @@ class GraphicBox extends Component {
       <div className="GraphicBox">
         <div className="gStructure">
           <Chart
+            className="graphicBars"
             options={this.state.options}
             series={this.state.series}
             type="bar"
-            height={240}
+            height={300}
           />
         </div>
       </div>
